29b92920773c1aac636b44ade0fb357987c775e8
Ghost_chu <2908803755@qq.com>
2022-04-06 18:55:31 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# HuskHomesPerServerLimit
+
+## 简述
+
+限制单个服务器上玩家创建的 Home 创建数量，但不影响全局。
+
+## 权限
+
+```text
+huskhomesperserverlimit.bypass
+```
---
+++ src/main/java/com/ghostchu/huskhomesperserverlimit/HuskHomesPerServerLimit.java
@@ -0,0 +1,63 @@
+package com.ghostchu.huskhomesperserverlimit;
+
+import me.william278.huskhomes2.HuskHomes;
+import me.william278.huskhomes2.api.HuskHomesAPI;
+import me.william278.huskhomes2.api.events.PlayerSetHomeEvent;
+import me.william278.huskhomes2.teleport.points.Home;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class HuskHomesPerServerLimit extends JavaPlugin implements Listener {
+    private String serverName;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        saveDefaultConfig();
+        this.serverName = HuskHomes.getSettings().getServerID();
+        HuskHomes huskHomes = (HuskHomes) Bukkit.getPluginManager().getPlugin("HuskHomes");
+        if (huskHomes != null) {
+            if (!huskHomes.isEnabled()) {
+                getLogger().warning("HuskHomes is not enabled, disabling...");
+                Bukkit.getPluginManager().disablePlugin(this);
+                return;
+            }
+        }
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        this.serverName = HuskHomes.getSettings().getServerID();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onHomeSet(PlayerSetHomeEvent event) {
+        Player player = event.getPlayer();
+        List<Home> playerHomes = HuskHomesAPI.getInstance().getHomes(player);
+        List<Home> playerHomesLocal = playerHomes.stream().filter(home -> home.getServer().equals(serverName)).collect(Collectors.toList());
+        int ownsOnThisServer = playerHomes.size();
+        // 区分大小写
+        boolean sameHomeNameOnLocalAlreadyExists = playerHomesLocal.stream().anyMatch(home->home.getName().equals(event.getHome().getName()));
+        if (!sameHomeNameOnLocalAlreadyExists)
+            ownsOnThisServer++;
+        int limitLocal = getConfig().getInt("slots");
+        if (ownsOnThisServer > limitLocal && player.hasPermission("huskhomesperserverlimit.bypass")) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("message.out-of-slot")).replace("{slots}",String.valueOf(limitLocal)));
+        }
+    }
+}
